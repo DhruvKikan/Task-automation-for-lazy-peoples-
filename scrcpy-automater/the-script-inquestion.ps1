6fce0dcac980740d78e53c224130c830c7a13872
DhruvKikan <110284707+DhruvKikan@users.noreply.github.com>
2024-09-27 11:09:51 +0530

    Update the-script-in-question.ps1

--- scrcpy-automater/the-script-inquestion.ps1
+++ scrcpy-automater/the-script-inquestion.ps1
@@ -1,8 +1,8 @@
-Write-Host "Starting script, v2 by Shrigma and Skibidiboss"
+Write-Host "Starting script, v2 by Shrigma and Skibidib0ss"
 # $output = bash -c "/mnt/c/Programming/Tools/scrcpy/adb.exe devices | tail -n 2 | head -n 1 | awk '{print \`$1}'"
 $out = C:\Programming\Tools\scrcpy\adb.exe devices | Select-Object -Last 2  | Select-Object -Skiplast 1
 $output = ($out -split "\s")[0]
-Write-Host "Man this took some time, but we are finally BACCCCCCK"
+Write-Host "Man this took some time, but we are finally BACKKK"
 if ($output -eq "162e71aa") {
     Write-Host "Device Found, starting scrcpy"
     Start-Sleep -Seconds 3
